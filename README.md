8c844413cb58f5c8f45e45573c53a585ae27e018
ErickCelestino <erickcelestimo@gmail.com>
2023-06-22 00:06:14 -0300

    Crate brick paddle and implematation brick

---
+++ README.md
@@ -0,0 +1,5 @@
+# Repository focused in games
+
+- Link from tutorial video <a href="https://www.youtube.com/watch?v=7bejSTim38A&ab_channel=freeCodeCamp.org">M</a>
+
+- Tempo do video 1:06:03 
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,68 @@
-// Start here
-console.log("Start here!");
+import { CanvasView } from "./view/canvas-view";
+import { Ball,Paddle, Brick  } from "./sprites";
+// Images
+import PADDLE_IMAGE from './images/paddle.png';
+import BALL_IMAGE from './images/ball.png';
+// Level and colors
+import {
+    PADDLE_SPEED,
+    PADDLE_WIDTH,
+    PADDLE_HEIGHT,
+    PADDLE_STARTX,
+    BALL_SPEED,
+    BALL_SIZE,
+    BALL_STARTX,
+    BALL_STARTY
+} from './setup';
+// Helpers
+import { createBricks } from "./helpers";
+
+let gameOver = false;
+let score = 0;
+
+function setGameOver(view: CanvasView) {
+    view.drawInfo('Game over!');
+    gameOver = false;
+}
+
+function setGameWin(view: CanvasView) {
+    view.drawInfo('Game won!');
+    gameOver = false;
+}
+
+function gameLoop(
+    view: CanvasView,
+    bricks: Brick[],
+    paddle: Paddle,
+    // ball: Ball
+) {
+    view.clear();
+    view.drawBricks(bricks);
+
+    requestAnimationFrame(() => gameLoop(view, bricks, paddle));
+}
+
+function startGame(view: CanvasView) {
+    // Reset displays
+    score = 0;
+    view.drawInfo('');
+    view.drawScore(0);
+    //Create all bricks
+    const bricks = createBricks();
+    // Create a paddle
+    const paddle = new Paddle(
+        PADDLE_SPEED,
+        PADDLE_WIDTH,
+        PADDLE_HEIGHT,
+        {
+            x: PADDLE_STARTX,
+            y: view.canvas.height - PADDLE_HEIGHT - 5
+        },
+        PADDLE_IMAGE
+    )
+
+    gameLoop(view, bricks, paddle);
+}
+
+const view = new CanvasView('#playField');
+view.initStartButton(startGame);
